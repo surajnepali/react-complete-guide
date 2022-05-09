4b5411bf72c29739349510b7d8bf9fe04ad833ec
surajnepali <surajnepali7896@gmail.com>
2022-05-09 12:43:56 +0545

    result isnot similar to that of video

--- src/App.js
+++ src/App.js
@@ -1,88 +1,106 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-// class App extends Component {
-//   state = {
+class App extends Component {
+  state = {
+    persons: [
+      {name: 'Suraj', age: 24 },
+      {name: 'Susmita', age: 23 },
+      {name: 'Susan', age: 18 }
+    ],
+    otherState: 'sone other value'
+  }
+
+  switchEventHandler = (newName) => {
+    // console.log('Was Clicked!!');
+    //DON'T DO THIS: this.state.person[0].name = 'Asmita';
+    this.setState ( {
+      persons: [
+        {name: newName, age: 22 },
+        {name: 'Susmita', age: 23 },
+        {name: 'Susan', age: 19 }
+      ]
+    }) 
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        { name: 'Suraj', age: 24 },
+        { name: event.target.value, age: 23 },
+        { name: 'Susan', age: 18 }
+      ]
+    } )
+  }
+
+  render() {
+    return (
+      //JSX
+      <div className="App">
+        <h1>Hi, I'm React App.</h1>
+        <p>This is really working!!!</p>
+        <button onClick={() => this.switchEventHandler('Asmita!!')}>Switch Name</button> 
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchEventHandler.bind(this, 'Kanchhu')}
+          changed={this.nameChangedHandler} >My Hobbies: Dance</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
+      </div>
+    );
+
+    
+    //Children means 'all' that are nested in between this <div></div>
+    //creating elements with 4 arguments. 
+    //Here, className: 'App' jun chha ni yo chai div in-line class='' wala concept ho haii
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m React App'));
+  }
+}
+
+// const app = (props) => {
+//   const [personsState, setPersonsState] = useState({
 //     persons: [
 //       {name: 'Suraj', age: 24 },
 //       {name: 'Susmita', age: 23 },
 //       {name: 'Susan', age: 18 }
-//     ],
-//     otherState: 'sone other value'
-//   }
+//     ]
+//   }); 
 
-//   switchEventHandler = () => {
-//     // console.log('Was Clicked!!');
-//     //DON'T DO THIS: this.state.person[0].name = 'Asmita'
-//     this.setState ( {
+//   const [otherState, setOtherState] = useState ('some other values'); 
+
+//   console.log(personsState,otherState);
+
+//   const switchEventHandler = () => {
+//     //whenever we switch 'personsState' to 'setPersonsSate' it replaces all the values. So,
+//     //we have to make sure that all our old data are included with our new values. 
+//     setPersonsState ({
 //       persons: [
 //         {name: 'Asmita', age: 22 },
 //         {name: 'Susmita', age: 23 },
 //         {name: 'Susan', age: 19 }
 //       ]
-//     }) 
-//   }
-
-//   render() {
-//     return (
-//       //JSX
-//       <div className="App">
-//         <h1>Hi, I'm React App.</h1>
-//         <p>This is really working!!!</p>
-//         <button onClick={this.switchEventHandler}>Switch Name</button> 
-//         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-//         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Dance</Person>
-//         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-//       </div>
-//     );
-
-    
-//     //Children means 'all' that are nested in between this <div></div>
-//     //creating elements with 4 arguments. 
-//     //Here, className: 'App' jun chha ni yo chai div in-line class='' wala concept ho haii
-//     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m React App'));
-//   }
-// }
-
-const app = (props) => {
-  const [personsState, setPersonsState] = useState({
-    persons: [
-      {name: 'Suraj', age: 24 },
-      {name: 'Susmita', age: 23 },
-      {name: 'Susan', age: 18 }
-    ]
-  }); 
-
-  const [otherState, setOtherState] = useState ('some other values'); 
-
-  console.log(personsState,otherState);
-
-  const switchEventHandler = () => {
-    //whenever we switch 'personsState' to 'setPersonsSate' it replaces all the values. So,
-    //we have to make sure that all our old data are included with our new values. 
-    setPersonsState ({
-      persons: [
-        {name: 'Asmita', age: 22 },
-        {name: 'Susmita', age: 23 },
-        {name: 'Susan', age: 19 }
-      ]
-    });
-  };
+//     });
+//   };
 
      
-  return (
-    //JSX
-    <div className="App">
-      <h1>Hi, I'm React App.</h1>
-      <p>This is really working!!!</p>
-      <button onClick={switchEventHandler}>Switch Name</button> 
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Dance</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
-    </div>
-  );
+//   return (
+//     //JSX
+//     <div className="App">
+//       <h1>Hi, I'm React App.</h1>
+//       <p>This is really working!!!</p>
+//       <button onClick={switchEventHandler}>Switch Name</button> 
+//       <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+//       <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Dance</Person>
+//       <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+//     </div>
+//   );
   
-}
+// }
 
-export default app; 
+export default App; 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,8 +6,9 @@ const person = (props) => { //arrow function
     //return <p>I'm a person and I'm {Math.floor(Math.random() * 30)} years old.</p>
     return (
         <div>
-            <p>I'm {props.name} and I'm {props.age} years old!</p>
-            <p>{props.children}</p>
+            <p onclick={props.click}>I'm {props.name} and I'm {props.age} years old!</p>
+            <p>{props.children}</p>   
+            {/* <input type="text" onchange={props.changed} value={props.name}></input>     */}
         </div>
         
     );
